ef711de098c27da8f8ca07c1ee935a46ab0839a7
Clovis_Az400 <clovisdev2022@gmail.com>
2024-01-13 18:27:21 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace TrataArquivoNetFrameWorl
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			LeituraArquivo();
+			RegrasNegocio();
+			EscritaArquivo();
+		}
+
+		private static void LeituraArquivo()
+		{
+			Console.WriteLine($"Inicio LeituraArquivo -> {DateTime.Now.ToString("MM'/'dd'/'yyyy HH':'mm':'ss.fff")}");
+			//Console.WriteLine("LeituraArquivo");
+			//RegrasNegocio();
+			Thread.Sleep(1001);
+		}
+		private static void RegrasNegocio()
+		{
+			Console.WriteLine($"Inicio RegrasNegocio -> {DateTime.Now.ToString("MM'/'dd'/'yyyy HH':'mm':'ss.fff")}");
+			//Console.WriteLine("RegrasNegocio");
+			//EscritaArquivo();
+			Thread.Sleep(1011);
+		}
+		private static void EscritaArquivo()
+		{
+			Console.WriteLine($"Inicio EscritaArquivo -> {DateTime.Now.ToString("MM'/'dd'/'yyyy HH':'mm':'ss.fff")}");
+			//	Console.WriteLine("EscritaArquivo");
+			Thread.Sleep(1021);
+			Console.ReadLine();
+		}
+	}
+}
